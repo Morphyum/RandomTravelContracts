a97eb884625fa98ecc32edf30de648fb896c8828
Morphyum <zion@c-schwabe.de>
2018-05-18 19:13:18 +0200

    *firstupload

---
+++ RandomTravelContracts/RandomTravelContracts/HolderClasses.cs
@@ -0,0 +1,18 @@
+﻿using BattleTech;
+using BattleTech.Framework;
+
+namespace RandomTravelContracts {
+    public struct PotentialContract {
+        // Token: 0x040089A4 RID: 35236
+        public ContractOverride contractOverride;
+
+        // Token: 0x040089A5 RID: 35237
+        public Faction employer;
+
+        // Token: 0x040089A6 RID: 35238
+        public Faction target;
+
+        // Token: 0x040089A7 RID: 35239
+        public int difficulty;
+    }
+}
---
+++ RandomTravelContracts/RandomTravelContracts/RandomTravelContracts.cs
@@ -0,0 +1,13 @@
+﻿using Harmony;
+using System.Reflection;
+
+namespace RandomTravelContracts {
+    public class RandomTravelContracts {
+        public static void Init() {
+            var harmony = HarmonyInstance.Create("de.morphyum.RandomTravelContracts");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+
+
+}
